8f3d5c55f192dc7855a9a2b3ee2a3fd9bd6f167e
calvin44 <calvin.fredicson@gmail.com>
2023-10-05 23:50:23 +0800

    refactor: adjust to fit Robotiive Shell format

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
-from fuzzywuzzy import fuzz, process
+from fuzzywuzzy import fuzz
+from sys import argv, exit, stdout
 import json
 
 
-def fuzzy_ratio(search_string, template):
+stdout.reconfigure(encoding='utf-8')
+
+
+def fuzzy_ratio(search_string: str, template: str) -> int:
     ratio = fuzz.ratio(search_string, template)
     return ratio
 
 
-def read_json_to_dict(file_path):
+def read_json_to_dict(file_path: str) -> list:
     with open(file_path, 'r', encoding="utf-8") as file:
         json_data = json.load(file)
     return json_data
 
 
-def fuzzy_search_from_all_result(query_string, all_result):
+def fuzzy_search_from_all_result(query_string, json_data) -> list:
+    all_result = json.loads(json_data)
     match_result_list = []
     for recognized_rectangle in all_result:
         ocr_text = recognized_rectangle["text"]
@@ -26,13 +31,16 @@ def fuzzy_search_from_all_result(query_string, all_result):
     return result
 
 
-def main(json_file_path, query_string):
-    json_data = read_json_to_dict(json_file_path)
+def main(json_data, query_string) -> None:
     find_string = fuzzy_search_from_all_result(query_string, json_data)
-    print(find_string)
+    print(json.dumps(find_string))
 
 
 if __name__ == "__main__":
-    json_file_path = r"C:\Users\Calvin Fredicson\Documents\Personal\Python\FuzzySearch\google_vision_words.json"
-    query_string = "Component Name"
-    main(json_file_path, query_string)
+    # json_file_path = r"C:\Users\Calvin Fredicson\Documents\Personal\Python\FuzzySearch\google_vision_words.json"
+    # query_string = "Component Name"
+    if len(argv) != 3:
+        exit(1)
+    query_string = argv[1]
+    json_data = argv[2]
+    main(json_data, query_string)
